d9c68d9fb5934c51dc483c165b6a305397ddad87
CongLap <lapp04552@gmail.com>
2026-05-11 17:14:39 +0700

    WIP: save current work before merging Hien branch

---
+++ Button_Segmented.Client/Components/Grid/HqGridTypes.cs
@@ -0,0 +1,13 @@
+namespace Button_Segmented.Client.Components.Grid;
+
+public enum HqGridMode
+{
+    ReadOnly,
+    Editable
+}
+
+public enum HqGridSize
+{
+    Md,
+    Sm
+}
---
+++ Button_Segmented.Client/Components/Pagination/HqPagerTypes.cs
@@ -0,0 +1,14 @@
+namespace Button_Segmented.Client.Components.Pagination;
+
+public enum HqPagerStyle
+{
+    Style1,
+    Style2
+}
+
+public enum HqPagerSize
+{
+    Md,
+    Sm,
+    Xs
+}
